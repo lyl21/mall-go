5d9d92d7059e815839344dbbb67d50e2744af187
lll <1091459024@qq.com>
2026-05-29 16:51:18 +0800

    修复(model/mall): GoodsSpu.Version struct tag修复
    1. GoodsSpu.Version gorm tag缺少右引号，导致GORM解析错误
    2. 该bug会导致乐观锁version字段无法正确映射到数据库列

--- server/model/mall/goods_spu.go
+++ server/model/mall/goods_spu.go
@@ -16,13 +16,13 @@ type GoodsSpu struct {
 	PicUrls        string          `json:"picUrls" gorm:"column:pic_urls;type:varchar(1024);not null;default:'';comment:商品图片"`
 	Shelf          int             `json:"shelf" gorm:"column:shelf;type:char(2);not null;default:0;comment:是否上架（1是 0否）"`
 	Sort           int             `json:"sort" gorm:"column:sort;not null;default:0;comment:排序字段"`
-	SalesPrice     *float64         `json:"salesPrice" gorm:"column:sales_price;type:decimal(10,2);comment:销售价格"`
-	MarketPrice    *float64         `json:"marketPrice" gorm:"column:market_price;type:decimal(10,2);comment:市场价"`
-	CostPrice      *float64         `json:"costPrice" gorm:"column:cost_price;type:decimal(10,2);comment:成本价"`
+	SalesPrice     *float64        `json:"salesPrice" gorm:"column:sales_price;type:decimal(10,2);comment:销售价格"`
+	MarketPrice    *float64        `json:"marketPrice" gorm:"column:market_price;type:decimal(10,2);comment:市场价"`
+	CostPrice      *float64        `json:"costPrice" gorm:"column:cost_price;type:decimal(10,2);comment:成本价"`
 	Stock          int             `json:"stock" gorm:"column:stock;not null;default:0;comment:库存"`
 	SalesNum       int             `json:"salesNum" gorm:"column:sales_num;default:0;comment:销量"`
 	DelFlag        string          `json:"delFlag" gorm:"column:del_flag;type:char(2);not null;default:'0';comment:逻辑删除标记"`
-	Version        int             `json:"version" gorm:"column:version;not null;default:0;comment:版本号（乐观锁）`
+	Version        int             `json:"version" gorm:"column:version;not null;default:0;comment:版本号（乐观锁）"`
 	CreateTime     common.DateTime `json:"createTime" gorm:"column:create_time;autoCreateTime;comment:创建时间"`
 	UpdateTime     common.DateTime `json:"updateTime" gorm:"column:update_time;autoUpdateTime;comment:最后更新时间"`
 	// 关联
